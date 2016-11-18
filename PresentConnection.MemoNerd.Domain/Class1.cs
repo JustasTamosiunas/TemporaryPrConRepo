87f4b3104c90903880b4ca5fe834d4cdecb15d59
Justas Tamošiūnas <runjustis@gmail.com>
2016-11-18 23:03:02 +0200

    Add project files.

---
+++ PresentConnection.MemoNerd.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentConnection.MemoNerd.Domain
+{
+    public class Class1
+    {
+    }
+}
