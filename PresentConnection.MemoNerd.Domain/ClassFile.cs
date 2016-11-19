9b9221530b1c44417b6a7f5ccb856f6ea5abd74c
Krisiukėnas Pijus <PS00533B>
2016-11-19 09:57:02 +0200

    Created cardlist class

--- PresentConnection.MemoNerd.Domain/ClassFile.cs
+++ PresentConnection.MemoNerd.Domain/ClassFile.cs
@@ -16,4 +16,11 @@ namespace PresentConnection.MemoNerd.Domain
         string word;
         string language; //ISO codes
     }
+
+    public class CardList 
+    {
+        List<string> Category;
+        List<Card> Cards;
+        
+    }
 }
